f99b2ed12cfa2028bf449b83d8ce8585d9621b93
Fatom Al-Ghanmi <fatom.alghanmi@triosstudent.com>
2025-10-29 10:16:19 -0400

    Initial Commit

---
+++ DatePickeraDemo/DatePickeraDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DatePickeraDemoApp.swift
+//  DatePickeraDemo
+//
+//  Created by Fatom on 2025-10-29.
+//
+
+import SwiftUI
+
+@main
+struct DatePickeraDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DatePickeraDemoTests/DatePickeraDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  DatePickeraDemoTests.swift
+//  DatePickeraDemoTests
+//
+//  Created by Fatom on 2025-10-29.
+//
+
+import Testing
+@testable import DatePickeraDemo
+
+struct DatePickeraDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
